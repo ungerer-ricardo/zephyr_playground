54a72fb2cb3c0549cdb41e19676b7ad36757c190
Ricardo Ungerer <ungerer.ricardo@gmail.com>
2024-05-12 14:12:21 +0000

    app: simplest blinking led working

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Simple instructions to build from scratch:
 ```
 $ west init
 $ west update
-$ west build
+$ west build  -- -DEXTRA_DTC_OVERLAY_FILE=board.overlay
 ```
 
 The `west` subcommands for flashing and debugging are not yet tested.
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,28 @@
-#include  <zephyr/kernel.h>
 #include <iostream>
+
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+#define LED0_NODE DT_ALIAS(led0)
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 int main(void) 
 { 
-    while (1) 
-    {
-        std::cout << "hello c++" << std::endl;
-        k_msleep(100U); // Sleep for 100 ms. 
+    if (!gpio_is_ready_dt(&led)) {
+        return 0;
+    }
+
+    if(gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) {
+        return 0;
+    }
+
+    while (1) {
+        int ret = gpio_pin_toggle_dt(&led);
+        if (ret < 0) {
+            return ret;
+        }
+        k_msleep(1000U);
     }
     return 0;
 }
