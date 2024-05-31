6cc860df28cf2f713dd5fca56eb25fc377371c7b
Ricardo Ungerer <ungerer.ricardo@gmail.com>
2024-05-31 18:00:40 +0100

    app: cycle through all 64 leds in the matrix

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/display.h>
 
 #define LED0_NODE DT_ALIAS(led0)
 
@@ -17,8 +19,42 @@ int main(void)
         return 0;
     }
 
+    const struct device* display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+    if(!device_is_ready(display_dev)) {
+        std::cout << "Display not ready...\n";
+        return 0;
+    }
+    display_set_pixel_format(display_dev, PIXEL_FORMAT_MONO01);
+
+    display_buffer_descriptor desc;
+    desc.buf_size = 1;
+    desc.height = 1;
+    desc.width = 1;
+    desc.pitch = 1;
+    uint8_t buf[1] = {1};
+
+    int i =0;
+    int j =0;
     while (1) {
-        int ret = gpio_pin_toggle_dt(&led);
+        buf[0] = 0;
+        int ret = display_write(display_dev,i,j,&desc, buf);
+        if (ret < 0) {
+            std::cout << "Error writing to display\n";
+        }
+        i++;
+        if (i >= 8 ) {
+            i = 0;
+            j++;
+            if (j >= 8)
+                j = 0;
+        }
+        buf[0] = 1;
+        ret = display_write(display_dev,i,j,&desc, buf);
+        if (ret < 0) {
+            std::cout << "Error writing to display\n";
+        }
+        
+        ret = gpio_pin_toggle_dt(&led);
         if (ret < 0) {
             return ret;
         }
